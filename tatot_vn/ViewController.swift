291fa04bc2c5526f5e81482c9e34a272dd648c05
nhan92 <nhannt92@gmail.com>
2015-04-21 09:33:49 +0700

    Initial Commit

---
+++ tatot_vn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tatot_vn
+//
+//  Created by Nhan Nguyen on 4/21/15.
+//  Copyright (c) 2015 Nhan Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
